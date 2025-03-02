2f190ef2fb93e3122c7cd8e9c1d0c082bd475f17
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-03-02 16:16:46 -0500

    Reglas fraude redis

--- src/main/java/com/banquito/paymentprocessor/validafraude/banquito/service/ReglaFraudeService.java
+++ src/main/java/com/banquito/paymentprocessor/validafraude/banquito/service/ReglaFraudeService.java
@@ -3,7 +3,6 @@ package com.banquito.paymentprocessor.validafraude.banquito.service;
 import com.banquito.paymentprocessor.validafraude.banquito.dto.ReglaFraudeDTO;
 import com.banquito.paymentprocessor.validafraude.banquito.exception.ReglaFraudeNotFoundException;
 import com.banquito.paymentprocessor.validafraude.banquito.repository.ReglaFraudeRepository;
-
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 import java.util.List;
@@ -12,57 +11,59 @@ import java.util.List;
 @Slf4j
 public class ReglaFraudeService {
 
-    private final ReglaFraudeRepository reglaFraudeRepository;
+    private final ReglaFraudeRepository redisRepository;
 
-    public ReglaFraudeService(ReglaFraudeRepository reglaFraudeRepository) {
-        this.reglaFraudeRepository = reglaFraudeRepository;
+    public ReglaFraudeService(ReglaFraudeRepository redisRepository) {
+        this.redisRepository = redisRepository;
     }
 
     public List<ReglaFraudeDTO> obtenerTodas() {
         log.debug("Obteniendo todas las reglas de fraude del sistema");
-        return reglaFraudeRepository.findAll();
+        return redisRepository.findAll();
     }
 
     public ReglaFraudeDTO buscarPorCodigo(String codigoRegla) {
         log.debug("Buscando regla de fraude por código: {}", codigoRegla);
-        ReglaFraudeDTO regla = reglaFraudeRepository.findById(codigoRegla);
-        if (regla == null) {
+        ReglaFraudeDTO reglaRedis = redisRepository.findById(codigoRegla);
+        if (reglaRedis == null) {
             throw new ReglaFraudeNotFoundException(codigoRegla);
         }
-        return regla;
+        return reglaRedis;
     }
 
     public void registrarNueva(ReglaFraudeDTO reglaFraudeDTO) {
         log.debug("Registrando nueva regla de fraude en el sistema: {}", reglaFraudeDTO);
-        reglaFraudeRepository.save(reglaFraudeDTO);
+        redisRepository.save(reglaFraudeDTO);
     }
 
     public void actualizarExistente(String codigoRegla, ReglaFraudeDTO reglaFraudeDTO) {
         log.debug("Actualizando regla de fraude existente: {}", codigoRegla);
-        ReglaFraudeDTO existente = reglaFraudeRepository.findById(codigoRegla);
-        if (existente == null) {
+        ReglaFraudeDTO reglaExistente = redisRepository.findById(codigoRegla);
+        if (reglaExistente == null) {
             throw new ReglaFraudeNotFoundException(codigoRegla);
         }
+        
         reglaFraudeDTO.setCodReglaFraude(codigoRegla);
-        reglaFraudeRepository.save(reglaFraudeDTO);
+        redisRepository.save(reglaFraudeDTO);
     }
 
     public void eliminar(String codigoRegla) {
         log.debug("Eliminando regla de fraude del sistema: {}", codigoRegla);
-        ReglaFraudeDTO existente = reglaFraudeRepository.findById(codigoRegla);
-        if (existente == null) {
+        ReglaFraudeDTO reglaExistente = redisRepository.findById(codigoRegla);
+        if (reglaExistente == null) {
             throw new ReglaFraudeNotFoundException(codigoRegla);
         }
-        reglaFraudeRepository.deleteById(codigoRegla);
+        
+        redisRepository.deleteById(codigoRegla);
     }
 
     public List<ReglaFraudeDTO> obtenerReglasActivas() {
         log.debug("Obteniendo listado de reglas de fraude activas");
-        return reglaFraudeRepository.findByEstadoTrue();
+        return redisRepository.findByEstadoTrue();
     }
 
     public List<ReglaFraudeDTO> obtenerReglasActivasPorTipo(String tipoRegla) {
         log.debug("Obteniendo listado de reglas de fraude activas por tipo: {}", tipoRegla);
-        return reglaFraudeRepository.findByTipoReglaAndEstadoTrue(tipoRegla);
+        return redisRepository.findByTipoReglaAndEstadoTrue(tipoRegla);
     }
 } 
