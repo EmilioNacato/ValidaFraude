b7b30941fb1f2501e9116e22a04601182915edb7
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-03-04 10:36:26 -0500

    Reglas fraude redis 2

--- src/main/java/com/banquito/paymentprocessor/validafraude/banquito/service/MigracionService.java
+++ src/main/java/com/banquito/paymentprocessor/validafraude/banquito/service/MigracionService.java
@@ -6,16 +6,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 import java.util.List;
 import java.util.Set;
 
-/**
- * Servicio para migrar datos antiguos en Redis al nuevo formato.
- */
 @Service
 @Slf4j
 public class MigracionService {
@@ -29,9 +25,7 @@ public class MigracionService {
         this.objectRedisTemplate = objectRedisTemplate;
     }
 
-    /**
-     * Método para migrar datos una vez que la aplicación ha sido inicializada.
-     */
+
     @EventListener(ApplicationReadyEvent.class)
     public void migrarDatosAntiguos() {
         log.info("Iniciando proceso de migración de datos antiguos...");
@@ -44,7 +38,6 @@ public class MigracionService {
             
             log.info("Se encontraron {} claves antiguas para migrar", keysAntiguas.size());
             
-            // Obtener datos actuales
             List<ReglaFraudeDTO> reglasActuales = reglaFraudeRepository.findAll();
             log.info("Reglas actuales en el sistema: {}", reglasActuales.size());
             
